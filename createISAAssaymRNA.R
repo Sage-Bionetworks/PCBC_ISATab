dc8b25dfa9d614ae425f72e8b18818deafb7c95e
Kenneth Daily <kenneth.daily@sagebase.org>
2015-01-08 11:52:29 -0800

    Abstracted some files, fixed column naming.

--- createISAAssaymRNA.R
+++ createISAAssaymRNA.R
@@ -18,23 +18,72 @@ mrnaAll <- qr$collectAll()
 mrnaAll <- tbl_df(mrnaAll)
 colnames(mrnaAll) <- gsub("file\\.", "", colnames(mrnaAll))
 
-mrnaBam <- filter(mrnaAll, fileType=="bam", bamType=="mapped")
+# The raw data file
 mrnaFastq <- filter(mrnaAll, fileType=="fastq")
-mrnaFpkm <- filter(mrnaAll, fileType=="fpkm", fpkmType == "genes")
-# mrnaFpkmIso <- filter(mrnaAll, fileType=="fpkm", fpkmType == "isoforms")
 
-mrnaUse <- rbind(mrnaBam, mrnaFastq, mrnaFpkm)
-castToISA <- dcast(mrnaUse, UID + C4_Cell_Line_ID + Diffname_short ~ fileType,
+# Derived files
+mrnaBam <- filter(mrnaAll, fileType=="bam", bamType=="mapped")
+mrnaFpkmGene <- filter(mrnaAll, fileType=="fpkm", fpkmType == "genes")
+mrnaFpkmIso <- filter(mrnaAll, fileType=="fpkm", fpkmType == "isoform")
+
+## Convert separate file types to a subtype attribute
+mrnaBam$subType  <- "mapped"
+mrnaFastq$subType <- "fastq"
+mrnaFpkmGene$subType <- "genes"
+mrnaFpkmIso$subType <- "isoform"
+
+mrnaUse <- rbind(mrnaBam, mrnaFastq, mrnaFpkmGene, mrnaFpkmIso)
+
+mrnaUse$sample.name <- with(mrnaUse, paste(C4_Cell_Line_ID, Diffname_short))
+
+castToISA <- dcast(mrnaUse,
+                   sample.name + UID ~ fileType + subType,
                    value.var="name",
                    fun.aggregate=function(x) paste(x, collapse=","))
 
-castToISA <- filter(castToISA, bam != "", fastq != "", fpkm != "")
+# castToISA <- filter(castToISA, bam_mapped != "", fastq_fastq != "", fpkm_genes != "", fpkm_isoform != "")
 
-castToISA$fastq.source <- "Synapse"
-castToISA$bam.source <- "Synapse"
-castToISA$fpkm.source <- "Synapse"
+castToISA$fastq.comment <- paste("Synapse ID", mrnaUse$id[match(castToISA$fastq_fastq, mrnaUse$name)])
 
-castToISA$fastq.id <- mrnaUse$id[match(castToISA$fastq, mrnaUse$name)]
-castToISA$bam.id <- mrnaUse$id[match(castToISA$bam, mrnaUse$name)]
-castToISA$fpkm.id <- mrnaUse$id[match(castToISA$fpkm, mrnaUse$name)]
+castToISA$bam.trans.name <- "Alignment"
+castToISA$bam.comment <- paste("Synapse ID", mrnaUse$id[match(castToISA$bam_mapped, mrnaUse$name)])
 
+castToISA$fpkm_genes.trans.name <- "Cufflinks Genes"
+castToISA$fpkm_genes.comment <- paste("Synapse ID", mrnaUse$id[match(castToISA$fpkm_genes, mrnaUse$name)])
+
+castToISA$fpkm_isoform.trans.name <- "Cufflinks Isoforms"
+castToISA$fpkm_isoform.comment <- paste("Synapse", mrnaUse$id[match(castToISA$fpkm_isoform, mrnaUse$name)])
+
+castToISA <- tbl_df(castToISA)
+
+attrcols <- c("sample.name", "UID")
+assaycols <- c("fastq_fastq", "fastq.comment",
+               "bam.trans.name", "bam_mapped", "bam.comment",
+               "fpkm_genes.trans.name", "fpkm_genes", "fpkm_genes.comment",
+               "fpkm_isoform.trans.name", "fpkm_isoform", "fpkm_isoform.comment")
+
+colorder <- c(attrcols, assaycols)
+
+cols.rename <- c("Sample Name", "Assay Name",
+                 "Raw Data File", "Comment",
+                 'Data Transformation Name', 'Derived Data File', 'Comment',
+                 'Data Transformation Name', 'Derived Data File', 'Comment',
+                 'Data Transformation Name', 'Derived Data File', 'Comment')
+
+castToISA <- castToISA[, colorder]
+
+names(castToISA) <- cols.rename
+
+# Fix the duplicated column names
+colnames(castToISA) <- gsub(pattern="Comment.*", "Comment", colnames(castToISA))
+colnames(castToISA) <- gsub(pattern="Data Transformation Name.*", "Data Transformation Name", colnames(castToISA))
+colnames(castToISA) <- gsub(pattern="Derived Data File.*", "Derived Data File", colnames(castToISA))
+
+write.csv(castToISA, file="a_PCBC_all.csv", row.names=FALSE)
+
+synfileMe  <- File(path="createISAAssaymRNA.R", parentId="syn2814512")
+synfileMe <- synStore(synfileMe)
+
+synfile  <- File(path="a_PCBC_all.csv", parentId="syn2814512")
+synfile <- synStore(synfile, 
+                    executed=synfileMe$properties$id)
